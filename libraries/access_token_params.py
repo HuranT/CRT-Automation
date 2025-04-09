dbf633f631c6616e56b025f688a3f49623ecfa4e
Huran Tunc <h.tunc@elsevier.com>
2025-04-09 22:10:36 +0000

    Latest

--- libraries/access_token_params.py
+++ libraries/access_token_params.py
@@ -1,14 +1,14 @@
 import requests
 
-def get_access_token(username, password, client_id, client_secret):
+def get_access_token(username1, password1, client_id1, client_secret1):
     url = "https://test.salesforce.com/services/oauth2/token"
 
     payload = {
         "grant_type": "password",
-        "client_id": client_id,
-        "client_secret": client_secret,
-        "username": username,
-        "password": password
+        "client_id": client_id1,
+        "client_secret": client_secret1,
+        "username": username1,
+        "password": password1
     }
 
     response = requests.post(url, data=payload)
