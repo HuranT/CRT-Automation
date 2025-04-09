80dd3119f34c18153b6ca3d65e587d36ca2c3d00
Huran Tunc <h.tunc@elsevier.com>
2025-04-09 21:41:42 +0000

    new version 1

--- libraries/save_quote_payload.py
+++ libraries/save_quote_payload.py
@@ -1,4 +1,3 @@
-# Updated save_quote function to get credentials from Robot Framework suite variables
 import requests
 import access_token_params
 import quote_type
@@ -10,14 +9,8 @@ def save_quote(quotetype_name, account_id, contact_id, address_id, agreement_acr
     print(f"quotetype_name in save_quote function: {quotetype_name}")
     logger.info(f"Received is_split_pay: {is_split_pay}", also_console=True)
 
-    # Get suite variables (credentials)
-    username = ${USERNAME}
-    password = ${PASSWORD}
-    client_id = ${CLIENT_ID}
-    client_secret = ${CLIENT_SECRET}
-
-    # Get the access token using suite variables
-    access_token = access_token_params.get_access_token1(username, password, client_id, client_secret)
+    # Get the access token
+    access_token = access_token_params.get_access_token()
     if not access_token:
         print("Access token retrieval failed.")
         return None
